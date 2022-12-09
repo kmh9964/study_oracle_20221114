b3e37b0724c93a1249b17eb14bc2716a4b9e768d
kmh9964 <raki82@hanmail.net>
2022-12-09 17:59:42 +0900

    연습문제10-2 완료

--- 연습문제10-2.sql
+++ 연습문제10-2.sql
@@ -141,3 +141,125 @@ where ch.master_id = ca. character_id
 and ch.role_id = 1001;
 
 commit;
+
+
+select *
+from characters
+where email like '%jedai%';
+
+select *
+from roles
+where role_name = '제다이';--role_id 가 1001
+
+select character_name,email
+from characters
+where role_id = 1001
+order by character_name;
+
+select c.character_name,nvl(d.character_name,'제다이 중의 제다이') masters
+from characters c , characters d,roles r
+where c.master_id = d.character_id(+)
+and c.role_id = r.role_id
+and r.role_id = 1001 -- 일반조건
+order by character_name;
+
+select c.character_name,nvl(d.character_name,'제다이 중의 제다이') masters
+from characters c , characters d,roles r
+where c.master_id = d.character_id(+)
+and c.role_id = r.role_id
+and r.role_name = '제다이' -- 일반조건
+order by character_name;
+
+
+
+8.
+select c.character_name,c.email jedai_email, m.email master_email
+from characters c , characters m,roles r
+where c.master_id = m.character_id(+)
+and c.role_id = r.role_id
+and r.role_name = '제다이' -- 일반조건
+order by c. character_name;
+
+9.스타워즈 시리즈별로 출연한 배우의 수를 파악하고자 한다
+에피소드 이름과 출연 배우 수를 개봉연도 순으로 조회
+
+select *
+from star_wars;
+
+select *
+from characters;
+
+select *
+from roles;
+
+select *
+from casting;
+
+select s.episode_name , s.open_year
+from star_wars s,casting c
+where s.episode_id = c.episode_id
+group by episode_name, s.open_year
+order by 1;
+
+select s.episode_name, count(s.episode_name)"배우의 수", s.open_year
+from star_wars s,casting c
+where s.episode_id = c.episode_id
+group by episode_name, s.open_year
+order by 1;
+
+
+10.
+select ch.character_name 배역이름, ca.real_name 실제이름, count(* )
+from characters ch, casting ca
+where ch.character_id = ca.character_id
+group by ch.character_name , ca.real_name;
+
+11. 10번을 참고하여 출연 횟수가 많은 상위 3명의 배역명, 실명, 출연 횟수를 조회
+-- ROWNUM : 쿼리 실행 순서대로~ (상위, 하위) : 일종의 함수 ==> 의사 컬럼    vs  ROW_NUMBER()
+-- RANK() OVER(ORDER BY 절), DENSE_RANK() OVER(ORER BY절)
+-- 1,2,2,4,5,6...           vs   1,2,2,3,4,6...
+-- 동순위 다음 순위 건너뜀     vs   동순위 다음 순위도 표시
+-- 그룹함수의 조건 : HAVING에 표시
+
+SELECT  ch.character_name 배역이름, ca.real_name 실제이름, count(*) 출연횟수
+FROM    characters ch, casting ca
+WHERE   ch.character_id = ca.character_id
+GROUP BY ch.character_name, ca.real_name;
+
+-- 11-1. 교재에서 사용한 방법 : ROWNUM + 인라인 뷰 서브쿼리
+--
+SELECT ROWNUM ranking, e.*
+FROM    (   SELECT  ch.character_name 배역이름, ca.real_name 실제이름, count(*) 출연횟수
+            FROM    characters ch, casting ca
+            WHERE   ch.character_id = ca.character_id
+            GROUP BY ch.character_name, ca.real_name
+            ORDER BY 1 DESC ) e -- 인라인 뷰 : 실제로는 존재하지 않는 가상의 / 임시성 테이블 (메모리에서 처리)
+WHERE   ROWNUM <= 3;
+
+-- 11-2. RANK() 또는 DENSE_RANK() 사용한 방법
+SELECT  *
+FROM    (   SELECT  DENSE_RANK() OVER(ORDER BY COUNT(*)) ranking, ch.character_name 배역이름, ca.real_name 실제이름, count(*) 출연횟수
+            FROM    characters ch, casting ca
+            WHERE   ch.character_id = ca.character_id
+            GROUP BY ch.character_name, ca.real_name    )
+WHERE   ROWNUM <= 3;            
+
+
+
+
+12. 시리즈별 몇명의 배우가(=실제 배우) 출연 했는지 조회
+-- 에피소드 시리즈 번호, 에피소드 이름, 출연 배우의 수
+-- 출연 배우의 수가 많은 순으로 조회
+
+-- star_wars : 에피소드 정보
+-- casting : 실제배우 정보
+
+SELECT  s.episode_id, s.episode_name, count(*) actor_count
+FROM    star_wars s, casting c
+WHERE   s.episode_id = c.episode_id
+GROUP BY s.episode_id, s.episode_name
+--ORDER BY    actor_count DESC;
+ORDER BY 3 DESC;
+
+
+
